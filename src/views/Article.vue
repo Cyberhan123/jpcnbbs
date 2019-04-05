ccd80f55fd54c178de88f37fe7b53d843e4cc1a2
glx <1480059245@qq.com>
2019-04-05 21:13:29 +0800

    test

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -6,21 +6,47 @@
             left-arrow
             @click-left="returnBtn"></van-nav-bar>
     <el-card shadow="always" style="margin-bottom: 10px" >
-        <h3 class="el-card-title">胡萝卜不辣</h3>
         <div class="el-card-actile">
-            胡萝卜不辣胡萝卜不辣胡萝卜不辣胡萝卜
-            胡萝卜不辣胡萝卜不辣胡萝卜不辣胡萝卜
-            胡萝卜不辣胡萝卜不辣胡萝卜不辣胡萝卜
-            胡萝卜不辣胡萝卜不辣胡萝卜不辣胡萝卜
-            胡萝卜不辣胡萝卜不辣胡萝卜不辣胡萝卜
+
+
         </div>
-        <span class="el-card-uptime">2018-11-26 10:12:23</span>
+        <el-input
+                type="textarea"
+                :autosize="{ minRows: 2, maxRows: 4}"
+                placeholder="请输入内容"
+                v-model="textarea"
+        >
+        </el-input>
+        <el-button type="primary" size="mini" round class="published">发表</el-button>
+        <div class="clearBoth"></div>
+        <div class="comments" >
+            <div>
+                <img src="./../assets/headImage.png" class="headImage" alt="">
+                <div class="commentsName">dwqd</div>
+            </div>
+            <div class="clearBoth"></div>
+            <div class="commentsContext">dwqdqdqwdqdqwddqw</div>
+            <div class="clearBoth"></div>
+
+            <span class="el-card-up time">2018-11-26 10:12:23</span>
+        </div>
     </el-card>
+    <el-card>
+
+    </el-card>
+
+
 </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                textarea: '',
+                arr:[1,2,3,4,5,6,7,8,9,10]
+            }
+        },
         name: "Article",
         methods:{
             returnBtn(){
@@ -31,5 +57,35 @@
 </script>
 
 <style scoped>
-
+.commentsContext{
+    float: left;
+    margin-left: 2rem;
+}
+.headImage{
+    float: left;
+    width: 1.5rem;
+    height: 1.5rem;
+    overflow: hidden;
+    border-radius:50%;
+}
+.clearBoth{
+    clear: both;
+}
+.commentsName{
+    float: left;
+    display: inline-block;
+    margin-left: 0.5rem;
+    height: 1.5rem;
+    line-height: 1.5rem;
+    color: deepskyblue;
+}
+.published{
+    float: right;
+    margin-top: 0.5rem;
+}
+.comments{
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    border-bottom: 0.05rem gainsboro solid;
+}
 </style>
